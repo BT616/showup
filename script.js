930dc7e17b02bba3d964ed7220eb9563550b5600
BT616 <110855674+BT616@users.noreply.github.com>
2023-03-22 18:12:23 -0700

    got elements and wrote unhide function for display

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let sectionElement=document.getElementById('info');
-let h2Element=sectionElement.getElementsByTagName('h2')[0];
+let h2Element=sectionElement.getElementsByTagName('h2')[1];
+h2Element.onclick =showHide;
+
+function showHide(){
+if (this.nextElementSibling.hasAttribute('class') && this.nextElementSibling.getAttribute('class')=='hide') {
+this.nextElementSibling.removeAttribute('class');
+this.innerHTML='Show Less...'
+}else{
+    this.nextElementSibling.setAttribute('class','hide');
+    this.innerHTML='Show More...'
+}
+}
