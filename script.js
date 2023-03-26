567482533314cc9f2dfecb77dca45abda254617f
BT616 <110855674+BT616@users.noreply.github.com>
2023-03-25 19:16:45 -0700

    create element(p) and append review to page

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let sectionElement=document.getElementById('info');
 let h2Element=sectionElement.getElementsByTagName('h2')[1];
-h2Element.onclick =showHide;
+
+
+var review= document.getElementById('review');
+
+
+var submitBtn=document.getElementById('btn');
+submitBtn.addEventListener('click',(event)=> { 
+event.preventDefault()
+var createP= document.createElement('p');
+createP.append(review.value)
+document.getElementById('reviews').appendChild(createP)
+})
+
+
+
 
 function showHide(){
 if (this.nextElementSibling.hasAttribute('class') && this.nextElementSibling.getAttribute('class')=='hide') {
@@ -11,3 +25,5 @@ this.innerHTML='Show Less...'
     this.innerHTML='Show More...'
 }
 }
+
+h2Element.onclick =showHide;
