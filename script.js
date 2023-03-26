86d1dbfec55bb55e779cea79bfdbdc1d68a7e094
BT616 <110855674+BT616@users.noreply.github.com>
2023-03-25 19:21:51 -0700

    commits

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let sectionElement=document.getElementById('info');
 let h2Element=sectionElement.getElementsByTagName('h2')[1];
 
-
 var review= document.getElementById('review');
-
-
 var submitBtn=document.getElementById('btn');
+
+//add event listener to button, then creates p tag, grabs value from input field and appends to reviews id tag
 submitBtn.addEventListener('click',(event)=> { 
 event.preventDefault()
 var createP= document.createElement('p');
@@ -14,8 +13,9 @@ document.getElementById('reviews').appendChild(createP)
 })
 
 
-
-
+//finds attribute class and determines if its 'hide',
+//if it is itll show more of the paragraph that is originally hidden. 
+//will then remove the class(hide) to show the text.
 function showHide(){
 if (this.nextElementSibling.hasAttribute('class') && this.nextElementSibling.getAttribute('class')=='hide') {
 this.nextElementSibling.removeAttribute('class');
